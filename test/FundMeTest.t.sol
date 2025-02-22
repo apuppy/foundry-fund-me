daea3822ab13b6127bd157d7decddce02ec106be
lihongde <chinalihongde@gmail.com>
2025-02-22 23:15:25 +0800

    add testcase and deploy script

---
+++ test/FundMeTest.t.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../src/FundMe.sol";
+
+contract FundMeTest is Test {
+    FundMe fundMe;
+
+    function setUp() external {
+        fundMe = new FundMe();
+    }
+
+    function testMinimumUsdIsFive() external view {
+        assertEq(fundMe.MINIMUM_USD(), 5e18);
+    }
+
+    function testOwnerIsMsgSender() external view {
+        console.log("fundMe.i_owner(): ", fundMe.i_owner());
+        console.log("msg.sender: ", msg.sender);
+        // assertEq(fundMe.i_owner(), msg.sender);
+        assertEq(fundMe.i_owner(), address(this));
+    }
+}
